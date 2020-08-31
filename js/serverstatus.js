58cd355be00475b39f5b52526712c7219379f083
him41111 <33783119+him41111@users.noreply.github.com>
2020-08-31 22:48:13 +0800

    Update serverstatus.js

--- js/serverstatus.js
+++ js/serverstatus.js
@@ -72,7 +72,11 @@ function bytesToSize(bytes, precision, si)
 }
 
 function uptime() {
-    $.getJSON("https://sj.him411.top/stats.json", function(result) {
+    $.ajax({
+    url: 'https://sj.him411.top/stats.json',
+    type: 'get',
+    dataType: 'jsonp',
+});, function(result) {
       alert(result);
       var shstr = '<div class="col-lg-4 col-md-4 col-sm-4">'+
                        ' <div class="panel panel-block panel-block-sm panel-location">'+
